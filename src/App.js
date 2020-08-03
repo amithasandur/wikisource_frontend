874b45086ad3819dbe9f1d6d87d60e9fd4c55529
junhchen11 <junhchen11@gmail.com>
2020-08-02 20:09:29 -0500

    rest changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,162 @@
-
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
-import CustomPaginationActionsTable from './myTable';
-import ThreeColTable from "./my3RowTable";
-import FiveColTable from "./my5rowtable";
-import FourColTable from "./my4rowtable";
+import wiki from './wiki.png';
+import './App.css';
+
+import axios from 'axios';
+import { ActionExtension } from 'material-ui/svg-icons';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: 'ASDFASDFASDFASDF',
+			txt: '',
+			articleArray: [],
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	async handleSubmit(event) {
+		event.preventDefault();
+		var resp = await axios.get(
+			`http://127.0.0.1:5000//wikiarticles/${this.state.value}`
+		);
+		this.setState({ ...this.state, txt: resp.data.data[0].url });
+	}
+
+	async handleClick(event) {
+		var resp = await axios.get('http://127.0.0.1:5000/select');
+		this.setState({ ...this.state, article: resp.data });
+	}
+	async handleDelete(event) {
+		await axios.delete(
+			`http://127.0.0.1:5000//wikiarticles/${this.state.value}`
+		);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={wiki} className="App-logo" alt="logo" />
+					<p>WikiSource</p>
+					<form onSubmit={this.handleSubmit}>
+						<label>
+							Name:
+							<input
+								type="text"
+								value={this.state.value}
+								onChange={this.handleChange}
+							/>
+						</label>
+						<input type="submit" value="Submit" />
+					</form>
+					<p>{this.state.txt}</p>
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={this.handleClick}
+					>
+						{' '}
+						Show Articles{' '}
+					</Button>
+					<a
+						className="App-link"
+						href="https://en.wikipedia.org/wiki/Wikipedia:Multiyear_ranking_of_most_viewed_pages"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Most popular Wikipedia articles
+					</a>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
+/* const getArticlesSuccess = (res) => {
+	return {
+		type: GET_ARTICLES_SUCCESS,
+		payload: res,
+	};
+};
+const getArticlesFailed = (err) => {
+	return {
+		type: GET_ARTICLES_FAILED,
+		payload: err,
+	};
+};
+const getArticlesRequest = () => {
+	return {
+		type: GET_ARTICLES_REQUEST,
+	};
+};
+export const getArticles = () => (dispatch) =>
+	new Promise((resolve, reject) => {
+		dispatch(getArticlesRequest());
+
+		let body = {
+			crossDomain: true,
+		};
+
+		axios
+			.get('http://127.0.0.1:5000' + '/wikiarticles', body)
+			.then((res) => {
+				dispatch(getArticlesSuccess(res.data.data));
+				resolve(res.data.data);
+			})
+			.catch((err) => {
+				dispatch(getArticlesFailed(err));
+				reject(err);
+			});
+	}); */
+/* 0155e1bde4a43d44888f084b554131e2bf5eb573
+
+    return (
+        <div>
+        <Button variant="contained" color="primary" disableElevation>
+            Delete
+        </Button>
+            <Button variant="contained" color="primary" disableElevation>
+                Search
+            </Button>
+            <InputBase
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+        />
+    <SearchIcon />
+            <InputBase
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <SearchIcon />
+    <CustomPaginationActionsTable></CustomPaginationActionsTable>
+            <ThreeColTable></ThreeColTable>
+            <FourColTable></FourColTable>
+            <FiveColTable></FiveColTable>
+
+        </div>
+    );
+}
+ */
 //a
-const useStyles = makeStyles((theme) => ({
+/* const useStyles = makeStyles((theme) => ({
     grow: {
         flexGrow: 1,
     },
@@ -71,116 +218,4 @@ const useStyles = makeStyles((theme) => ({
             display: 'none',
         },
     },
-}));
-
-export default function DisableElevation() {
-    const classes = useStyles();
-import wiki from './wiki.png';
-import './App.css';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={wiki} className="App-logo" alt="logo" />
-				<p>WikiSource</p>
-				<input type="text" className="input" placeholder="Search..." />
-				<Button
-					variant="contained"
-					color="primary"
-					onClick={clickButton()}
-				>
-					{' '}
-					Search{' '}
-				</Button>
-				<a
-					className="App-link"
-					href="https://en.wikipedia.org/wiki/Wikipedia:Multiyear_ranking_of_most_viewed_pages"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Most popular Wikipedia articles
-				</a>
-			</header>
-		</div>
-	);
-}
-function clickButton() {
-	axios.get('http://127.0.0.1:5000/select').then((resp) => {
-		console.log(resp.data);
-	});
-}
-/* const getArticlesSuccess = (res) => {
-	return {
-		type: GET_ARTICLES_SUCCESS,
-		payload: res,
-	};
-};
-const getArticlesFailed = (err) => {
-	return {
-		type: GET_ARTICLES_FAILED,
-		payload: err,
-	};
-};
-const getArticlesRequest = () => {
-	return {
-		type: GET_ARTICLES_REQUEST,
-	};
-};
-export const getArticles = () => (dispatch) =>
-	new Promise((resolve, reject) => {
-		dispatch(getArticlesRequest());
-
-		let body = {
-			crossDomain: true,
-		};
-
-		axios
-			.get('http://127.0.0.1:5000' + '/wikiarticles', body)
-			.then((res) => {
-				dispatch(getArticlesSuccess(res.data.data));
-				resolve(res.data.data);
-			})
-			.catch((err) => {
-				dispatch(getArticlesFailed(err));
-				reject(err);
-			});
-	}); */
->>>>>>> 0155e1bde4a43d44888f084b554131e2bf5eb573
-
-    return (
-        <div>
-        <Button variant="contained" color="primary" disableElevation>
-            Delete
-        </Button>
-            <Button variant="contained" color="primary" disableElevation>
-                Search
-            </Button>
-            <InputBase
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-        />
-    <SearchIcon />
-            <InputBase
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-            />
-            <SearchIcon />
-    <CustomPaginationActionsTable></CustomPaginationActionsTable>
-            <ThreeColTable></ThreeColTable>
-            <FourColTable></FourColTable>
-            <FiveColTable></FiveColTable>
-
-        </div>
-    );
-}
+})); */
